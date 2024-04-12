d031bc3e12bcbaebd12ff6ad68f247d1da421bca
Guillaume Verger <guillaume.verger@artelys.com>
2024-04-12 18:09:38 +0200

    Allow for parallel runs
    Probably needs some configuration options there
    
    Signed-off-by: Guillaume Verger <guillaume.verger@artelys.com>

--- csa-runner-app/src/main/java/com/farao_community/farao/swe_csa/app/RequestConfiguration.java
+++ csa-runner-app/src/main/java/com/farao_community/farao/swe_csa/app/RequestConfiguration.java
@@ -3,6 +3,7 @@ package com.farao_community.farao.swe_csa.app;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.function.Function;
 
@@ -11,9 +12,11 @@ public class RequestConfiguration {
 
     @Bean
     public Function<Flux<byte[]>, Flux<byte[]>> request(RequestService requestService) {
-        return csaRequestFlux -> csaRequestFlux
-                .map(requestService::launchCsaRequest)
-                .log();
+        return csaRequestFlux -> csaRequestFlux.parallel()
+                                               .runOn(Schedulers.newBoundedElastic(100, 1, "rao-compute"))
+                                               .map(requestService::launchCsaRequest)
+                                               .sequential()
+                                               .log();
     }
 
 }
