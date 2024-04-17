fa8de89e5d8816fa6acbf1466e6971b4e6ef5e7c
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2024-04-17 13:48:00 +0200

    check that all assessed elements associated to both PT-ES border and FR-ES border are secure

--- csa-runner-app/src/main/java/com/farao_community/farao/swe_csa/app/dichotomy/SweCsaRaoValidator.java
+++ csa-runner-app/src/main/java/com/farao_community/farao/swe_csa/app/dichotomy/SweCsaRaoValidator.java
@@ -19,10 +19,13 @@ import com.farao_community.farao.swe_csa.api.exception.CsaInternalException;
 import com.farao_community.farao.swe_csa.api.resource.CsaRequest;
 import com.farao_community.farao.swe_csa.app.FileExporter;
 import com.farao_community.farao.swe_csa.app.FileImporter;
+import com.powsybl.iidm.network.Country;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.loadflow.LoadFlow;
 import com.powsybl.loadflow.LoadFlowParameters;
+import com.powsybl.openrao.commons.Unit;
 import com.powsybl.openrao.data.cracapi.Crac;
+import com.powsybl.openrao.data.cracapi.cnec.FlowCnec;
 import com.powsybl.openrao.data.raoresultapi.RaoResult;
 import com.powsybl.openrao.data.raoresultjson.RaoResultImporter;
 import com.powsybl.openrao.monitoring.voltagemonitoring.VoltageMonitoring;
@@ -33,6 +36,9 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.net.URL;
 import java.time.OffsetDateTime;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class SweCsaRaoValidator {
@@ -64,16 +70,36 @@ public class SweCsaRaoValidator {
                 raoResult = voltageMonitoring.runAndUpdateRaoResult(LoadFlow.find().getName(), LoadFlowParameters.load(), 1); // TODO number of LF in parallel
             }
 
-            // TODO when csa glsk is ready
-            if (withAngleMonitoring) {
-                //AngleMonitoring angleMonitoring = new AngleMonitoring(crac, network, raoResult, CimGlskDocument.importGlsk());
-                //raoResult = angleMonitoring.runAndUpdateRaoResult(LoadFlow.find().getName(), LoadFlowParameters.load(), 1, OffsetDateTime.parse(csaRequest.getBusinessTimestamp()));
-            }
+            // TODO when csa glsk is ready add withAngleMonitoring check to raoResult
 
-            return DichotomyStepResult.fromNetworkValidationResult(raoResult, raoResponse);
+            boolean allCnecsOnBothDichotomyBordersAreSecured = isSafeForBothBorders(network, crac, raoResult);
+            boolean secure = raoResult.isSecure() && allCnecsOnBothDichotomyBordersAreSecured;
+
+            return DichotomyStepResult.fromNetworkValidationResult(raoResult, raoResponse, secure);
         } catch (RuntimeException | IOException e) {
             throw new CsaInternalException("RAO run failed. Nested exception: " + e.getMessage());
         }
+    }
+
+    boolean isSafeForBothBorders(Network network, Crac crac, RaoResult raoResult) {
+        //TODO : implement association between cnecs and borders (CSA-67)
+        Set<FlowCnec> frEsFlowCnecs = crac.getFlowCnecs().stream()
+            .filter(flowCnec -> flowCnec.getLocation(network).contains(Optional.of(Country.FR)))
+            .collect(Collectors.toSet());
+        Set<FlowCnec> ptEsFlowCnecs = crac.getFlowCnecs().stream()
+            .filter(flowCnec -> flowCnec.getLocation(network).contains(Optional.of(Country.PT)))
+            .collect(Collectors.toSet());
+
+           return hasNoFlowCnecNegativeMargin(raoResult, frEsFlowCnecs) && hasNoFlowCnecNegativeMargin(raoResult, ptEsFlowCnecs);
+    }
+
+    boolean hasNoFlowCnecNegativeMargin(RaoResult raoResult, Set<FlowCnec> flowCnecs) {
+        for (FlowCnec flowCnec : flowCnecs) {
+            if (raoResult.getMargin(flowCnec.getState().getInstant(), flowCnec, Unit.MEGAWATT) < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private RaoRequest buildRaoRequest(String timestamp, String taskId, Network network, String cracUrl, String raoParametersUrl) {
