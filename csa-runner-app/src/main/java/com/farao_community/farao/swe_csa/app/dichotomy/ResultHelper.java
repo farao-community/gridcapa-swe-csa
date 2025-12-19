12b4dc3dda9d7fd0bb07a6813a72456b65acef3f
Thomas Bouquet <thomas.bouquet@rte-france.com>
2025-12-19 09:03:50 +0100

    fix: invert signs

--- csa-runner-app/src/main/java/com/farao_community/farao/swe_csa/app/dichotomy/ResultHelper.java
+++ csa-runner-app/src/main/java/com/farao_community/farao/swe_csa/app/dichotomy/ResultHelper.java
@@ -57,17 +57,17 @@ public class ResultHelper {
             case "PT-ES" -> {
                 double value = Math.abs(index.getPtEsLowestSecureStep().getLeft());
                 CounterTradeRangeAction ctRaPtes = findCounterTradingAction(ctActions, Country.PT, Country.ES);
-                resultMap.put(ctRaPtes, new CounterTradeRangeActionResult(ctRaPtes.getId(), -value, flowCnecs));
+                resultMap.put(ctRaPtes, new CounterTradeRangeActionResult(ctRaPtes.getId(), value, flowCnecs));
 
                 CounterTradeRangeAction ctRaEsPt = findCounterTradingAction(ctActions, Country.ES, Country.PT);
-                resultMap.put(ctRaEsPt, new CounterTradeRangeActionResult(ctRaEsPt.getId(), value, flowCnecs));
+                resultMap.put(ctRaEsPt, new CounterTradeRangeActionResult(ctRaEsPt.getId(), -value, flowCnecs));
             }
             case "FR-ES" -> {
                 double value = Math.abs(index.getFrEsLowestSecureStep().getLeft());
                 CounterTradeRangeAction ctRaFrEs = findCounterTradingAction(ctActions, Country.FR, Country.ES);
-                resultMap.put(ctRaFrEs, new CounterTradeRangeActionResult(ctRaFrEs.getId(), -value, flowCnecs));
+                resultMap.put(ctRaFrEs, new CounterTradeRangeActionResult(ctRaFrEs.getId(), value, flowCnecs));
                 CounterTradeRangeAction ctRaEsFr = findCounterTradingAction(ctActions, Country.ES, Country.FR);
-                resultMap.put(ctRaEsFr, new CounterTradeRangeActionResult(ctRaEsFr.getId(), value, flowCnecs));
+                resultMap.put(ctRaEsFr, new CounterTradeRangeActionResult(ctRaEsFr.getId(), -value, flowCnecs));
             }
             default -> throw new IllegalArgumentException("Unsupported border: " + border);
         }
