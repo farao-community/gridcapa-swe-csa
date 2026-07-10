5c9ac8dc48d43c644684ab7b6c98e9d9b5da2910
Sharyal Zafar <sharyal.zafar@artelys.com>
2026-07-10 18:45:44 +0200

    remove MW not required
    Signed-off-by: Sharyal Zafar <sharyal.zafar@artelys.com>

--- csa-runner-app/src/main/java/com/farao_community/farao/swe_csa/app/dichotomy/CounterTradingValues.java
+++ csa-runner-app/src/main/java/com/farao_community/farao/swe_csa/app/dichotomy/CounterTradingValues.java
@@ -3,6 +3,6 @@ package com.farao_community.farao.swe_csa.app.dichotomy;
 public record CounterTradingValues(double ptEsCt, double frEsCt) {
 
     public String print() {
-        return String.format("PT-ES-scaled-by-%s-MW_and_FR-ES-scaled-by-%s-MW", Math.round(ptEsCt), Math.round(frEsCt));
+        return String.format("PT-ES-scaled-by-%s_and_FR-ES-scaled-by-%s", Math.round(ptEsCt), Math.round(frEsCt));
     }
 }
